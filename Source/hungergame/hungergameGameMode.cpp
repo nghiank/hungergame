aedda64bf5efda2d3fef5cf4398d112243703054
nghia <nghiank@hotmail.com>
2017-01-15 13:45:05 -0800

    Initial commit

---
+++ Source/hungergame/hungergameGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "hungergame.h"
+#include "hungergameGameMode.h"
+#include "hungergameHUD.h"
+#include "hungergameCharacter.h"
+
+AhungergameGameMode::AhungergameGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AhungergameHUD::StaticClass();
+}
