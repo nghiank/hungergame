bdbe12606a3ec623276edadfa7a0ddc31ad5e2e8
nghia <nghiank@hotmail.com>
2017-01-30 20:54:23 -0800

    Add ChooseNextWaypoint

--- Source/hungergame/hungergame.Build.cs
+++ Source/hungergame/hungergame.Build.cs
@@ -6,6 +6,6 @@ public class hungergame : ModuleRules
 {
 	public hungergame(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
