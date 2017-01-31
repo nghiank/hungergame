7fae782cb29d33d3dd0d53a98e37bd78b0c3f97c
nghia <nghiank@hotmail.com>
2017-01-30 22:00:23 -0800

    Migrate ChooseNextWaypoint into cpp

--- Source/hungergame/ChooseNextWaypoint.cpp
+++ Source/hungergame/ChooseNextWaypoint.cpp
@@ -2,9 +2,14 @@
 
 #include "hungergame.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("Not implemented yet"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	Index++;
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i, %s"), Index, *IndexKey.SelectedKeyName.ToString());
 	return EBTNodeResult::Succeeded;
 }
--- Source/hungergame/ChooseNextWaypoint.h
+++ Source/hungergame/ChooseNextWaypoint.h
@@ -12,8 +12,9 @@ UCLASS()
 class HUNGERGAME_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };
